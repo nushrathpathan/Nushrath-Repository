d6780d5f1a835d8e4197130365b75d9a6232490b
nushrathpathan <56912391+nushrathpathan@users.noreply.github.com>
2020-08-12 15:12:24 +0530

    Disk_Space_Report_Multiple_Servers
    Disk_Space_Report_Multiple_Servers

---
+++ Disk_Space_Report_Multiple_Servers.ps1
@@ -0,0 +1,11 @@
+﻿$DiskSizeReport = @()
+$computers = Get-Content C:\Scripts\servername.txt
+$computers | ForEach {
+$Disks = Get-WmiObject win32_logicaldisk -ComputerName $_ -Filter "Drivetype=3" -ErrorAction SilentlyContinue |
+Select-Object @{Label = "Server Name";Expression = {$_.SystemName}},
+@{Label = "Drive Letter";Expression = {$_.DeviceID}},@{Label = "Total Capacity (GB)";Expression = {"{0:N1}" -f( $_.Size / 1GB)}},
+@{Label = "Used Space (GB)";Expression = {(Round($_.Size /1GB,2)) - (Round($_.FreeSpace /1GB,2))}},
+@{Label = "Free Space (GB)";Expression = {"{0:N1}" -f( $_.Freespace / 1GB ) }},@{Label = "Free Space (%)"; Expression = {"{0:P0}" -f ($_.freespace/$_.size) }}
+$DiskSizeReport += $Disks
+} 
+$DiskSizeReport | Export-CSV "C:\Scripts\DiskSizeReport.csv" -NoTypeInformation -Encoding UTF8
